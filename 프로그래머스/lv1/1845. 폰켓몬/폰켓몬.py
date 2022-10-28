ed3a803385e26e940827d40f16d514aec675e778
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-10-29 00:52:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.42 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,17 +1,23 @@
+# def solution(nums):
+#     # 가질 수 있는 포켓몬 수
+#     get_num = int(len(nums)/2)
+    
+#     # 중복 제거하였을 때, 종류가 몇 개인지 확인
+#     set_nums = set(nums)
+#     set_num = len(set_nums)
+    
+#     # 폰켓몬 종류 수의 최댓값
+#     answer = 0
+    
+#     if get_num >= set_num:
+#         answer = set_num
+#     else:
+#         answer = get_num
+    
+#     return answer
 def solution(nums):
-    # 가질 수 있는 포켓몬 수
-    get_num = int(len(nums)/2)
     
-    # 중복 제거하였을 때, 종류가 몇 개인지 확인
-    set_nums = set(nums)
-    set_num = len(set_nums)
-    
-    # 폰켓몬 종류 수의 최댓값
-    answer = 0
-    
-    if get_num >= set_num:
-        answer = set_num
+    if int(len(nums)/2) >= len(set(nums)):
+        return len(set(nums))
     else:
-        answer = get_num
-    
-    return answer
+        return int(len(nums)/2)
