3422edd77084d874038ef4da138c6995c048f9b2
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-10-29 22:14:58 +0900

    [level 2] Title: 전화번호 목록, Time: 98.36 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,3 +1,18 @@
+# def solution(phone_book):
+
+#     # 순서 정렬하여 접두사가 0번 위치에 있도록 설정
+#     phone_book = sorted(phone_book)
+    
+#     # 접두사를 제외한 나머지 번호에서 접두사가 포함되었는지 확인
+#     answer = True
+
+#     for i in range(len(phone_book)-1):
+#         if len(phone_book[i]) < len(phone_book[i+1]):
+#             if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
+#                 answer = False
+#                 break
+
+#     return answer
 def solution(phone_book):
 
     # 순서 정렬하여 접두사가 0번 위치에 있도록 설정
@@ -9,7 +24,7 @@ def solution(phone_book):
     for i in range(len(phone_book)-1):
         if len(phone_book[i]) < len(phone_book[i+1]):
             if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
-                answer = False
+                return False
                 break
-
-    return answer
+        
+    return True
