b776dbb5c5b9295d0ac3b35d184084311d194a52
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-11-16 11:52:44 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,12 +1,11 @@
 def solution(keyinput, board):
-    ax0 = [x for x in range(board[0]//2*-1, board[0]//2+1)]
-    ax1 = [y for y in range(board[1]//2*-1, board[1]//2+1)]
+    ax0 = [board[0]//2*-1, board[0]//2]
+    ax1 = [board[1]//2*-1, board[1]//2]
     
     x = 0 # 처음 좌표 (x)
     y = 0 # 처음 좌표 (y)
     
     for k in keyinput:
-        
         if k == "left":
             if x <= ax0[0]:
                 pass
