0c8c02d1c7c91985ff7a8631e4e4f2703e0c57de
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-02 17:30:44 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,4 +1,3 @@
 def solution(numlist, n):
     s_numlist = sorted(numlist, reverse=True)
-    answer = sorted(s_numlist, key=lambda x:abs(n-x), reverse=False)
-    return answer
+    return sorted(s_numlist, key=lambda x:abs(n-x), reverse=False)
