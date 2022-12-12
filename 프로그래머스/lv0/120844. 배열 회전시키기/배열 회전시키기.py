e9df7e5bb1607163cccdb030d7bedcfe2d67129a
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-12 23:51:51 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,10 @@
+def solution(numbers, direction):
+    
+    if direction == 'left':
+        nums = numbers[1:]
+        nums.append(numbers[0])
+    else:
+        nums = numbers[:-1]
+        nums.insert(0, numbers[-1])
+    
+    return nums
