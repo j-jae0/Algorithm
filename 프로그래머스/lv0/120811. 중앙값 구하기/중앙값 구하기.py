e9202c29d20ced8b7ca10bbba1702b68991b7e42
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-16 21:35:30 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.64 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,14 +1,14 @@
-# import pandas as pd
+import pandas as pd
 # import numpy as np
 
-# def solution(array):
-#     arr = pd.Series(array) 
-#     return arr.median()
-
 def solution(array):
-    s_arr = sorted(array)
-    print(s_arr)
-    if len(s_arr)%2 == 0:
-        return int((s_arr[len(s_arr)//2] + s_arr[(len(s_arr)-1)//2])/2)
-    else:
-        return s_arr[len(s_arr)//2]
+    arr = pd.Series(array) 
+    return arr.median()
+
+# def solution(array):
+#     s_arr = sorted(array)
+#     print(s_arr)
+#     if len(s_arr)%2 == 0:
+#         return int((s_arr[len(s_arr)//2] + s_arr[(len(s_arr)-1)//2])/2)
+#     else:
+#         return s_arr[len(s_arr)//2]
