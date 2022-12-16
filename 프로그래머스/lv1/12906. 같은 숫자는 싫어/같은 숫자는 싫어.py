07aa93c378e3ea3d810a2d7ff5e5ff0fada107d5
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-16 10:20:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 41.02 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,14 +1,24 @@
+# def solution(arr):
+#     answer = []
+#     a = 10 # 없는 숫자 넣어줌
+    
+#     for num in arr:        
+#         if a != num:
+#             answer.append(num)
+#         else:
+#             pass        
+#         a = num
+    
+#     return answer
+
 def solution(arr):
     answer = []
     a = 10
     
-    for num in arr:
-        
+    for num in arr:        
         if a != num:
             answer.append(num)
-        else:
-            pass
-        
+
         a = num
     
     return answer
