9bbcf71ae34ba3cedb54e453d11e0c31ac7f1358
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-16 20:28:44 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 9.89 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,11 @@
+def solution(num1, num2):
+#     answer = None
+#     # 두 수가 같으면 1 반환, 그 외엔 -1 반환
+#     if num1 == num2:
+#         answer = 1
+#     else:
+#         answer = -1
+    
+#     return answer
+
+    return 1 if num1==num2 else -1
