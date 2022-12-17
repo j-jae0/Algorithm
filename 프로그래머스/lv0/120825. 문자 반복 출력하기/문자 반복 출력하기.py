212fe99ecf627a47bd2be19d0efc481c050321e6
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-18 06:26:23 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,8 +1,4 @@
 def solution(my_string, n):
-    # def fun(x):
-    #     return x*n
-    # return "".join(list(map(fun, my_string)))
-    answer = ''
-    for i in range(len(my_string)):
-        answer += my_string[i]*n
-    return answer
+    def fun(x):
+        return x*n
+    return "".join(list(map(fun, my_string)))
