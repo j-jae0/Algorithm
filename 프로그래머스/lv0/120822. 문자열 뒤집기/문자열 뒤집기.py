62364dff76733e4f9f9e484e81038fb80014c886
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-18 05:36:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,8 @@
 def solution(my_string):
 
-    return "".join([my_string[i] for i in range(-1, (len(my_string)+1)*-1, -1)])
+    # return "".join([my_string[i] for i in range(-1, (len(my_string)+1)*-1, -1)])
+    answer = ""
+    for i in range(-1, (len(my_string)+1)*-1, -1):
+        answer += my_string[i]
+        
+    return answer
