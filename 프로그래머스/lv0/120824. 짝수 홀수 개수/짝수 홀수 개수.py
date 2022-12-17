6ad52eb29ee54100d8373e91515390792b38167d
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2022-12-18 05:28:49 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,9 @@
 def solution(num_list):
+    # 짝수 개수
+    even_list = [e for e in num_list if e%2==0]
+    even_cnt = len(even_list)
+    # 홀수 개수
+    odd_list = [o for o in num_list if o%2!=0]
+    odd_cnt = len(odd_list)
     
-    return [len([x for x in num_list if x%2==0]), len([y for y in num_list if y%2!=0])]
+    return [even_cnt, odd_cnt]
