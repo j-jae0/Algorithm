172f0a85f29491eb6a0ad1f5d777790e318c2c00
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-03-20 12:05:18 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,7 +1,2 @@
 def solution(n):
-    answer = 2
-    for m in range(1, n):
-        if n == m*m:
-            answer = 1
-            break
-    return answer
+    return 1 if (n ** 0.5).is_integer() else 2
