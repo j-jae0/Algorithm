e039d5acbbabe75b648071cc8a44cc8289b9a99d
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-04-10 10:10:52 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,7 +1,4 @@
 def solution(num, k):
     str_num = str(num)
     str_k = str(k)
-    if str_k not in str_num:
-        return -1
-    else: 
-        return str_num.index(str_k) + 1
+    return -1 if str_k not in str_num else str_num.index(str_k) + 1
