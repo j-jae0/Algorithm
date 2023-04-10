fd25e21a16a0d607313efb7ae85e93ee84777145
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-04-10 10:12:41 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,9 +1,3 @@
 def solution(n, numlist):
-    answer = []
-    
-    # 나누어 떨어지는 값만 리스트에 담기
-    for num in numlist:
-        if num % n == 0:
-            answer.append(num)
-            
+    answer = [num for num in numlist if num % n == 0]      
     return answer
