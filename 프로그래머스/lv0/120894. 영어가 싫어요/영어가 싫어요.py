291ecdee4aba6828975c27cf5539b1cfeaddeb89
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-04-10 09:29:02 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,17 +1,5 @@
 def solution(numbers):
-    words_3 = {"one":"1", "two":"2", "six":"6"}
-    words_4 = {"zero":"0", "four":"4", "five":"5", "nine":"9"}
-    words_5 = {"three":"3", "seven":"7", "eight":"8"}
-    num = numbers
-    answer = ""
-    for _ in range(len(numbers)//3):
-        if num[:3] in words_3.keys():
-            answer += words_3[num[:3]]
-            num = num[3:]
-        elif num[:4] in words_4.keys():
-            answer += words_4[num[:4]]
-            num = num[4:]
-        elif num[:5] in words_5.keys():
-            answer += words_5[num[:5]]
-            num = num[5:]    
-    return int(answer)
+    num_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for i, n in enumerate(num_list):
+        numbers = numbers.replace(n, str(i))
+    return int(numbers)
