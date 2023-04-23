1a7c5e9acfab4075aef8e7f62dee0713b7fc76b1
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-04-24 04:09:24 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    answer1 = str(a) + str(b)
+    answer2 = str(b) + str(a)
+    
+    if answer1 == answer2:
+        return int(answer1)
+    elif int(answer1) > int(answer2):
+        return int(answer1)
+    return int(answer2)
