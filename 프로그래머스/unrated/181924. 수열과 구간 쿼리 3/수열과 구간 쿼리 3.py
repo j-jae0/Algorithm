6f10d56a9bd853c0c40bd60a4fedb29862e724a7
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-04-27 23:39:24 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    new_arr = arr
+    
+    for q in queries:
+        first_num = new_arr[q[0]]
+        second_num = new_arr[q[1]]
+        new_arr[q[0]] = second_num
+        new_arr[q[1]] = first_num
+
+    return new_arr
