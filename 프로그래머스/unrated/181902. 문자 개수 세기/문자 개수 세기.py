4a9f831c723d5f1870c3d73c14dd9ec51c9de91c
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-01 15:15:25 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,5 +1,6 @@
 def solution(my_string):
-    dic = {k:0 for k in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'}
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    dic = {k:0 for k in alpha}
     for s in my_string:
         dic[s] += 1
     return [v for v in dic.values()]
