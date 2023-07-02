1043b6dbd2468cb99f87c9c7b5a3381ddeab7cb3
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-03 00:40:13 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,11 +1,8 @@
-def operate(num_list):
+def solution(num_list):
     num = 1
     if len(num_list) > 10:
         return sum(num_list)
     else:
         for n in num_list: 
             num *= n
-        return num
-
-def solution(num_list):
-    return operate(num_list)
+    return num
