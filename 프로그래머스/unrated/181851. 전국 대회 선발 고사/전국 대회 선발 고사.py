f5d24a81f7f2c50822c1df416305089343d1982f
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-06 01:45:24 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,9 +1,9 @@
 def solution(rank, attendance):
-    dic = [(r, i) for i, r in enumerate(rank) if attendance[i]]
-    top3 = sorted([d[0] for d in dic])[:3]
+    rank_idx = [(r, i) for i, r in enumerate(rank) if attendance[i]]
+    top3 = sorted([ri[0] for ri in rank_idx])[:3]
     idx = []
     for t in top3:
-        for d in dic:
-            if d[0] == t:
-                idx.append(d[1])
+        for ri in rank_idx:
+            if ri[0] == t:
+                idx.append(ri[1])
     return 10000*idx[0] + 100*idx[1] + idx[2]
