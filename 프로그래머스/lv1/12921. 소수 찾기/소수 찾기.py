66211b3ce65cf92b1d0c4fe34c528a048b9406fa
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-13 19:10:30 +0900

    [level 1] Title: 소수 찾기, Time: 327.86 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,6 @@
-import numpy as np
-
 def solution(n):
-    l = []
+    num = set(range(2, n+1))
     for i in range(2, n+1):
-        prime = True
-        for j in range(2, int(np.sqrt(i))+1):
-            if i % j == 0:
-                prime = False
-                break
-        if prime:
-            l.append(i)
-    return len(l)
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+    return len(num)
