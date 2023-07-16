af1017d72665088784b4a8549e79f427b25467dc
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-17 05:40:20 +0900

    [unrated] Title: 문자열 나누기, Time: 1.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,27 +1,21 @@
 def solution(s):
-    l = []
     x, y = 0, 0
     first_alpha = False
-    new_string = ''
+    answer = 0
     
     for idx, alpha in enumerate(s):
         if not first_alpha:
             first_alpha = alpha
-            new_string += alpha
             x += 1
         elif alpha == first_alpha:
-            new_string += alpha
             x += 1
         else:
-            new_string += alpha
             y += 1
         
         if x == y:
-            l.append(new_string)
             x, y = 0, 0
             first_alpha = False
-            new_string = ''
-    
-    if len(new_string) > 0:
-        l.append(new_string)
-    return len(l)
+            answer += 1
+    if x != 0 or y != 0:
+        answer += 1
+    return answer
