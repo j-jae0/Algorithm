cdb58a1ac25630f704fba6ffde314af234d750f8
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-25 15:36:20 +0900

    [unrated] Title: 과일 장수, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,6 @@
 def solution(k, m, score):
     score = sorted([s for s in score if s <= k])[::-1]
     apple_box, box = list(), list()
-    
-    if len(score) // m == 1:
-        apple_box = score[:m]
-        return min(apple_box) * m
-    
     for s in score:
         box.append(s)
         if len(box) // m == 1:
