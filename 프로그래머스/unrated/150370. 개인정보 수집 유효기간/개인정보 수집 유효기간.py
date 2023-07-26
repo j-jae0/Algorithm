d114714e8ec2f207912999531c9d0df8aee96ca5
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-26 19:05:53 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.28 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,30 +1,10 @@
+def calculate_day(s):
+    y, m, d = map(int, s.split("."))
+    return y * 12 * 28 + m * 28 + d
+
 def solution(today, terms, privacies):
-    privacies = [p.split() for p in privacies]
-    terms = {t.split()[0]: int(t.split()[1]) for t in terms}
-    expiry = []
-    
-    for p in privacies:
-        join_year, join_month, join_date = p[0].split(".")
-        months = terms[p[1]]
-        if join_date == "01":
-            join_date = "28"
-            join_month = int(join_month) - 1
-        else:
-            join_date = str(int(join_date) - 1)
-        join_month = int(join_month) + months
-        if join_month % 12 == 0 and join_month > 12:
-            join_year = str(int(join_year) + (join_month - 12)//12)
-            join_month = "12"
-        elif join_month % 12 == 0:
-            join_month = "12"
-        else:
-            join_year = str(int(join_year) + int(join_month) // 12)
-            join_month = str(join_month % 12)
-        if len(join_month) < 2:
-            join_month = "0" + join_month
-        if len(join_date) < 2:
-            join_date = "0" + join_date
-        
-        expiry_date = join_year + "." + join_month + "." + join_date
-        expiry.append(expiry_date)
+    today = calculate_day(today)
+    terms = {t.split()[0]: int(t.split()[1])*28 for t in terms}
+    privacies = [[calculate_day(p.split()[0]), p.split()[1]] for p in privacies]
+    expiry = [p[0] + terms[p[1]] - 1 for p in privacies]
     return [i+1 for i, e in enumerate(expiry) if e < today]
