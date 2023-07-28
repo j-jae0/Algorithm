09aa3d3051c5793577eb2f56e122714e13d1f362
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-07-28 13:28:08 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,17 +1,7 @@
 def solution(X, Y):
     if len(set(X) & set(Y)) == 0:
         return "-1"
-    
-    dic_x = {x:0 for i, x in enumerate(list(X))}
-    dic_y = {y:0 for i, y in enumerate(list(Y))}
-    xy = []
-    for k in list(X): dic_x[k] += 1
-    for k in list(Y): dic_y[k] += 1
-    
-    for k, v in dic_x.items():
-        if k in dic_y.keys():
-            for i in range(min([v, dic_y[k]])):
-                xy.append(k)
-    if len(set(xy)) == 1:
-        return xy[0]
-    return "".join(sorted(xy)[::-1])
+    answer = ""
+    for n in range(10):
+        answer += str(n) * min([X.count(str(n)), Y.count(str(n))])
+    return answer[::-1] if len(set(answer)) > 1 else answer[0]
