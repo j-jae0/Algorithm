042960b633fd19cbd5baee1068c44985745dbfee
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-09-21 21:32:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.30 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,20 +1,4 @@
 def solution(participant, completion):
-    """
-    1) 참가자 명단에는 완주자 명단보다 1명 더 많다.
-    2) for문과 remove 를 같이 쓰면 효율이 떨어진다.    
-    3) 동명이인이 있을 수 있다. -> set으로 차집합, if not in 적합하지 않다.
-    """
-    
-    part = sorted(participant)
-    comp = sorted(completion)
-    answer = part[-1]
-
-    for i in range(len(comp)):
-        if part[i] == comp[i]:
-            pass
-        elif part[i] != comp[i]:
-            answer = part[i]
-            break
-
-    return answer
-        
+    completion.append('z'*20)
+    zips = zip(sorted(participant), sorted(completion))
+    return [z[0] for z in zips if z[0] != z[1]][0]
